35f34bf03ebdc89d87d892751ea91d876703370f
Conanas <tmhaigh@gmail.com>
2020-10-13 14:42:54 +1100

    updated comments

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
+// node modules
 const util = require("util");
 const path = require("path");
 const fs = require("fs");
 
+// path for employee output and save files
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const savedEmployeePath = path.join(OUTPUT_DIR, "saved-employees.json");
 
+// promisified async read and write
 const writeFileAsync = util.promisify(fs.writeFile);
 const readFileAsync = util.promisify(fs.readFile);
 
+// imported project modules and classes
 const render = require("./lib/htmlRenderer");
 const prompt = require("./lib/prompts");
 const createEmployee = require("./lib/create-employees");
 
+// adds another employee into the employee array
+// will keep adding employees into the array until the user prompts No for askAnother
 async function addEmployee(employeeArray) {
     try {
         let addingEmployee = true;
@@ -32,6 +38,12 @@ async function addEmployee(employeeArray) {
     }
 }
 
+// initialise function 
+// reads all the saved employees and puts them into an array
+// waits for user to add more employees
+// concats the saved employees and new employees and renders the html
+// writes the html to the team.html
+// writes all the new employees to the saved employees file
 async function init() {
     try {
         let employeeArray = [];
